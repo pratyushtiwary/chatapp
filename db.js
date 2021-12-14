b840bd5b316470d5d7292e28d0e9e6c53ffd6b63
Pratyush Tiwary <cantaloupeinc.1@gmail.com>
2021-12-14 18:02:21 +0530

    Changed config vars

--- db.js
+++ db.js
@@ -1,10 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize("chatapp","root","",{
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-    // logging: false
-});
+const db = new Sequelize("postgres://pskcszopqtsrot:57ccc506eb6568458b71e6a47c0d8095893baca69dda222c0f9b62ff5d42e8fd@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d51crngfdp14kr");
 
 module.exports = db;
