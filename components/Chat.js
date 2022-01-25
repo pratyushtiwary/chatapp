5f4bf948595433fad122658a07fc3337c2403838
Pratyush Tiwary <cantaloupeinc.1@gmail.com>
2022-01-25 10:25:02 +0530

    Fixed the date issue with send message

--- components/Chat.js
+++ components/Chat.js
@@ -7,146 +7,160 @@ import { TextField, IconButton, Icon } from "@material-ui/core";
 import Profile from "./Profile";
 import SocketContext from "./Socket";
 
-export default function Chat({ user, mobile, onBack, token, unblockUser, blockUser, onSendMsg, msgs, initialChatLoad }) {
-    const [userMsg, setUserMsg] = useState("");
-    const [openProfile, setOpenProfile] = useState(false);
-    const socket = useContext(SocketContext);
-    const chatElem = useRef(null);
-    const [currUser,setCurrUser] = useState(null);
+export default function Chat({
+  user,
+  mobile,
+  onBack,
+  token,
+  unblockUser,
+  blockUser,
+  onSendMsg,
+  msgs,
+  initialChatLoad,
+}) {
+  const [userMsg, setUserMsg] = useState("");
+  const [openProfile, setOpenProfile] = useState(false);
+  const socket = useContext(SocketContext);
+  const chatElem = useRef(null);
+  const [currUser, setCurrUser] = useState(null);
 
-    useEffect(()=>{
-        let cE = chatElem.current;
-        if(cE){
-            cE.scrollTo(0,cE.scrollHeight);
-        }    
-    },[msgs])
+  useEffect(() => {
+    let cE = chatElem.current;
+    if (cE) {
+      cE.scrollTo(0, cE.scrollHeight);
+    }
+  }, [msgs]);
 
-    useEffect(()=>{
-        setCurrUser(user);
-        function loadChats({chats}){
-            initialChatLoad(chats);
-        }
+  useEffect(() => {
+    setCurrUser(user);
+    function loadChats({ chats }) {
+      initialChatLoad(chats);
+    }
 
-        socket.emit("fetch-chats",{
-            email: user.email,
-            token: token.token
+    socket.emit("fetch-chats", {
+      email: user.email,
+      token: token.token,
+    });
+
+    socket.on("load-initial-chat", loadChats);
+    return () => {
+      socket.off("load-initial-chat", loadChats);
+    };
+  }, [token, socket, user]);
+
+  function sendMsg(e) {
+    e.preventDefault();
+    const time = new Date();
+    const month =
+      time.getMonth() + 1 > 9
+        ? time.getMonth() + 1
+        : "0" + (time.getMonth() + 1);
+    const hours = time.getHours() > 9 ? time.getHours() : "0" + time.getHours();
+    const minutes =
+      time.getMinutes() > 9 ? time.getMinutes() : "0" + time.getMinutes();
+    let finalTime =
+      time.getFullYear() +
+      "-" +
+      month +
+      "-" +
+      time.getUTCDate() +
+      "T" +
+      hours +
+      ":" +
+      minutes +
+      ":00Z";
+    if (userMsg !== "") {
+      if (socket) {
+        socket.emit("send-msg", {
+          to: user.email,
+          from: token.email,
+          msg: userMsg,
+          token: token.token,
         });
-
-        socket.on("load-initial-chat",loadChats)
-        return ()=>{
-            socket.off("load-initial-chat",loadChats)
-        }
-    },[token,socket,user])
-
-    function sendMsg(e){
-        e.preventDefault();
-        const date = new Date();
-        const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-        const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-        let finalTime = date.getUTCDate() + "-" + (date.getUTCMonth() + 1) + "-" + date.getFullYear() + " " + hours + ":"+minutes     
-        if(userMsg!==""){
-            if(socket){
-                socket.emit("send-msg",{
-                    to: user.email,
-                    from: token.email,
-                    msg: userMsg,
-                    token: token.token
-                })
-            }
-            let temp = {
-                msg: userMsg,
-                on: finalTime,
-                byMe: true
-            }
-            onSendMsg(temp);
-            setUserMsg("");
-        }
+      }
+      let temp = {
+        msg: userMsg,
+        on: finalTime,
+        byMe: true,
+      };
+      onSendMsg(temp);
+      setUserMsg("");
     }
+  }
 
-    function onBlock(){
-        if(user && socket){
-            socket.emit("blockUser",{
-                token: token.token,
-                to: user.email
-            });
-            blockUser(user);
-        }
+  function onBlock() {
+    if (user && socket) {
+      socket.emit("blockUser", {
+        token: token.token,
+        to: user.email,
+      });
+      blockUser(user);
     }
+  }
 
-    function onUnBlock(){
-        if(user && socket){
-            socket.emit("unblockUser",{
-                token: token.token,
-                to: user.email
-            });
-            unblockUser(user);
-        }
+  function onUnBlock() {
+    if (user && socket) {
+      socket.emit("unblockUser", {
+        token: token.token,
+        to: user.email,
+      });
+      unblockUser(user);
     }
+  }
 
-    function showProfile(){
-        setOpenProfile(true);
-    }
+  function showProfile() {
+    setOpenProfile(true);
+  }
 
-    return (
-        <>
-            <Profile
-                data = {currUser}
-                open={openProfile}
-                onDismiss = {()=>setOpenProfile(false)}
-                onBlock={onBlock}
-                onUnBlock={onUnBlock}
+  return (
+    <>
+      <Profile
+        data={currUser}
+        open={openProfile}
+        onDismiss={() => setOpenProfile(false)}
+        onBlock={onBlock}
+        onUnBlock={onUnBlock}
+      />
+      <div className={styles.chat}>
+        {!mobile && <Divider vertical />}
+        <div className={styles.mainChat}>
+          <InfoBox
+            data={currUser || {}}
+            mobile={mobile}
+            onBack={onBack}
+            onClick={showProfile}
+          />
+          <Divider />
+          <div className={styles.chats} ref={chatElem}>
+            {msgs.map((e, i) => (
+              <ChatBubble
+                msg={e.msg}
+                on={e.on}
+                byMe={e.byMe}
+                key={i}
+                index={i}
+              />
+            ))}
+          </div>
+          <form className={styles.sendMsgForm} onSubmit={sendMsg}>
+            <TextField
+              type="text"
+              placeholder="Type your message..."
+              variant="outlined"
+              className={styles.msgInput}
+              value={userMsg}
+              onChange={(e) => setUserMsg(e.target.value)}
             />
-            <div
-                className={styles.chat}
+            <IconButton
+              type="submit"
+              aria-label="Send Message"
+              className={styles.sendMsg}
             >
-                {
-                    !mobile && (
-                        <Divider vertical/>
-                    )
-                }
-                <div className={styles.mainChat}>
-                    <InfoBox
-                        data={currUser||{}}
-                        mobile={mobile}
-                        onBack={onBack}
-                        onClick={showProfile}
-                    />
-                    <Divider/>
-                    <div className={styles.chats} ref={chatElem}>
-                        {
-                            msgs.map((e,i)=>(
-                                <ChatBubble
-                                    msg={e.msg}
-                                    on={e.on}
-                                    byMe={e.byMe}
-                                    key={i}
-                                    index={i}
-                                />
-                            ))
-                        }
-                    </div>
-                    <form
-                        className={styles.sendMsgForm}
-                        onSubmit={sendMsg}
-                    >
-                        <TextField
-                            type="text"
-                            placeholder="Type your message..."
-                            variant="outlined"
-                            className={styles.msgInput}
-                            value={userMsg}
-                            onChange={(e)=>setUserMsg(e.target.value)}
-                        />
-                        <IconButton 
-                            type="submit"
-                            aria-label="Send Message"
-                            className={styles.sendMsg}
-                        >
-                            <Icon>send</Icon>
-                        </IconButton>
-                    </form>
-                </div>
-            </div>
-        </>
-    )
+              <Icon>send</Icon>
+            </IconButton>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
