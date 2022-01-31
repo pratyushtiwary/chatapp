8cc734f36259eaf548e90b76554831f4a64f9c7a
Pratyush Tiwary <cantaloupeinc.1@gmail.com>
2022-01-31 11:56:40 +0530

    Made sentiment analysis more accurate

--- controllers/analyzeSentiment.js
+++ controllers/analyzeSentiment.js
@@ -4,14 +4,16 @@ function analyze(msg) {
   try {
     const data = fs.readFileSync("./bag_of_words.json", "utf8");
     const bag_of_words = JSON.parse(data);
-    const final_msgs = msg.split(/\t|\s|\n/);
+    const final_msgs = msg.split(/[\W]/);
     if (final_msgs.length < 100) {
       let count = 0,
         score = 0;
       final_msgs.forEach((word) => {
-        if (bag_of_words[word.toLowerCase()] != undefined) {
-          count += 1;
-          score += parseInt(bag_of_words[word]);
+        if (word != "") {
+          if (bag_of_words[word.toLowerCase()] != undefined) {
+            count += 1;
+            score += parseInt(bag_of_words[word]);
+          }
         }
       });
       return [count, score];
