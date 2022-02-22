d3c304f1b5e15dec648ff0fe96b84f7b0d42e327
Pratyush Tiwary <cantaloupeinc.1@gmail.com>
2022-02-22 12:33:32 +0530

    Increased security

--- utils/cipher.js
+++ utils/cipher.js
@@ -2,28 +2,28 @@ const sha256 = require("crypto-js/sha256");
 const aes = require("crypto-js/aes");
 const cryptojs = require("crypto-js");
 
-const SALT = "3jw02i2qms";
+const SALT = "4530800rdsoxso92sakljs";
 
 function hash_password(password) {
-    return sha256(password.toString()).toString();
+  return sha256(password.toString()).toString();
 }
 
 function verify_password(password, hash) {
-    return hash_password(password) === hash;
+  return hash_password(password) === hash;
 }
 
-function encrypt(text,salt=SALT){
-    return aes.encrypt(text,salt).toString();
+function encrypt(text, salt = SALT) {
+  return aes.encrypt(text, salt).toString();
 }
 
-function decrypt(encryptedTxt,salt=SALT){
-    const bytes = aes.decrypt(encryptedTxt,salt);
-    return bytes.toString(cryptojs.enc.Utf8);
+function decrypt(encryptedTxt, salt = SALT) {
+  const bytes = aes.decrypt(encryptedTxt, salt);
+  return bytes.toString(cryptojs.enc.Utf8);
 }
 
 module.exports = {
-    hash_password,
-    verify_password,
-    encrypt,
-    decrypt
-}
+  hash_password,
+  verify_password,
+  encrypt,
+  decrypt,
+};
